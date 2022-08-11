56afc0a1955f39c659a36cecf74c673f329828d6
leandro <leandroandresvillegas@gmail.com>
2022-08-11 16:46:01 -0300

    Pug version

--- entregables/entregable5/server.js
+++ entregables/entregable5/server.js
@@ -10,15 +10,7 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
-// app.engine(
-//     'hbs',
-//     handlebars.engine({
-//         extname: '.hbs',
-//         defaultLayout: 'listaproductos.hbs',
-//         layoutsDir: __dirname + '/views',
-//         partialsDir: __dirname + '/views/partials'
-//     })
-// )
+
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -28,13 +20,6 @@ app.use(express.static('public'))
 app.get('/productos', async (req, res)=>{
     res.render('listaproductos', { listExist: true, list: await contenedor.getAll() })
 })
-
-
-
-
-// app.get('/productos', async (req, res) => {
-//     res.render('listaproductos', { listExist: true, list: await contenedor.getAll() })
-// })
 
 app.post('/productos', async (req, res) => {
     try {
@@ -53,7 +38,6 @@ app.post('/productos', async (req, res) => {
 
 
 })
-
 
 
 
